78654d21384cd9daa2fbf9ececa1f36416bc5065
Ali Kadir BAGCIOGLU <alikadirbagcioglu@gmail.com>
2019-06-09 10:46:59 +0300

    Create App.js

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react'
+
+class App extends Component {
+
+  render() {
+    return (
+      <React.Fragment>
+        <p>welcome to react</p>
+        <p>I'm first component</p>
+      </React.Fragment>
+    )
+  }
+}
+
+export default App
